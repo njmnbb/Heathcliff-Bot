04ac279770aeada2e1a37763b20fc9b7618657e7
njmnbb@gmail.com <njmnbb@gmail.com>
2022-02-01 13:58:07 -0600

    cant ebeliebve this

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // Require the necessary discord.js classes
 const { Client, Intents } = require('discord.js');
-const { token } = require('./config.json');
+const { token, channelId } = require('./config.json');
 const { getImage } = require("gocomics-api");
 const cron = require("node-cron");
 const greetings = require('./greetings');
@@ -18,18 +18,12 @@ client.on('ready', (client) => {
         tasks.splice(0, 1);
     }
     
-    const task = cron.schedule('0 0 8 * * *', () => {
+    const task = cron.schedule('0 55 13 * * *', () => {
         const date = new Date();
         const year = date.getFullYear();
         const month = date.getMonth() + 1;
         const day = date.getDate();
         let greeting = greetings[Math.floor(Math.random() * greetings.length) ];
-
-        fs.appendFile('logfile.log',
-            `Date being used: ${year} / ${month} / ${day} \n
-            Greeting being used: ${greeting}
-            \n\n-----------------------------------------------------------------\n\n`
-        );
 
         getImage({
             date: [year,month,day],
@@ -37,15 +31,10 @@ client.on('ready', (client) => {
             URLOnly: true
         }).then(
             (response) => {
-                client.channels.cache.get('789313283588489271').send(`@everyone
+                client.channels.cache.get(channelId).send(`@everyone
 Here's your daily Heathcliff comic! ${greeting}
                 
 ${response}`);
-            }
-        ).catch(err => {
-            fs.appendFile('logfile.log',
-                `Error occured while fetching comic: ${err}
-                \n\n-----------------------------------------------------------------\n\n`);
         });
     }, { scheduled: true, timezone: 'America/Chicago' });
 });
